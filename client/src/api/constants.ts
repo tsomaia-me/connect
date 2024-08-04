f17de79821cfad801d5ba436dc9773a8e2aa19e0
Tornike <tsomaia.me@gmail.com>
2024-08-04 18:52:40 +0400

    change base url

--- client/src/api/constants.ts
+++ client/src/api/constants.ts
@@ -1,2 +1,2 @@
-// export const BASE_URL = 'https://signaling.tsomaia.me'
-export const BASE_URL = 'http://localhost:8080'
+export const BASE_URL = 'https://signaling.tsomaia.me'
+// export const BASE_URL = 'http://localhost:8080'
